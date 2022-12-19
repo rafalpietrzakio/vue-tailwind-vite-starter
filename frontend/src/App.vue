ecb1d1530927f990daa071bbe12d6ba1826e29cd
Rafal Pietrzak <hello@quickness.io>
2022-12-19 17:50:56 +0100

    initial package with vue + tailwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import SampleDescription from "./components/SampleDescription.vue";
+</script>
+
+<template>
+  <div class="flex w-full justify-center">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <a href="https://tailwindcss.com/" target="_blank">
+      <img src="./assets/tailwind.svg" class="logo tailwind" alt="Tailwind logo" />
+    </a>
+  </div>
+
+  <HelloWorld msg="Vite + Vue + Tailwind" />
+
+  <SampleDescription text="This is a sample description text written and sent to the reusable component" />
+
+</template>
---
+++ frontend/src/components/SampleDescription.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+defineProps<{ text: string }>()
+</script>
+
+<template>
+  <div class="bg-gray-200 dark:bg-gray-900 w-full">
+    <p class="mt-20 p-20">{{ text }}</p>
+  </div>
+</template>
